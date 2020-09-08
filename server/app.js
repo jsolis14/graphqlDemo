23653d3b0e44375072d53ee42514da85b1db5337
Jesse Solis <jesse.solis0316@gmail.com>
2020-09-08 12:01:09 -0500

    setup graphiql

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const schema = require('./schema/schema')
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true,
 }))
+
 app.listen(4000, () => { console.log("Listening on port 4000") });
