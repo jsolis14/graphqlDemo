6e995f5381dcfce308ee6a072f6c3c0ac66f26d2
Jesse Solis <jesse.solis0316@gmail.com>
2020-09-14 15:40:39 -0500

    setup mongoose and mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://Jesse:password@localhost/gql-ninja', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Succesfully connected to database')
+});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
