28ec20a5de046cdabb26957ce5f3d18dfd208b3c
Jesse Solis <jesse.solis0316@gmail.com>
2020-09-15 11:41:48 -0500

    setup appolo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+
   );
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+export default function BookList() {
+
+    return (
+        <div>
+            <ul id="book-list">
+                <li>Book Name</li>
+            </ul>
+        </div>
+    )
+}
